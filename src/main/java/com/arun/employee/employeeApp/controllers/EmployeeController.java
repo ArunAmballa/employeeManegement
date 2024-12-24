cc52732c823eab9b07f17230855f832c0c344a6f
arunamballa <arunamballa24@gmail.com>
2024-12-23 20:19:35 -0800

    hello world api added

--- src/main/java/com/arun/employee/employeeApp/controllers/EmployeeController.java
+++ src/main/java/com/arun/employee/employeeApp/controllers/EmployeeController.java
@@ -1,4 +1,13 @@
 package com.arun.employee.employeeApp.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
+
+    @GetMapping
+    public String getEmployee() {
+        return "Hello World";
+    }
 }
